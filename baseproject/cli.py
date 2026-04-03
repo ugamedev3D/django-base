69fb34825ebb5226b8a056daef62deae0cd497e6
ugamedev3D <ugamedev08@gmail.com>
2026-04-04 00:07:22 +0800

    cli update

--- baseproject/cli.py
+++ baseproject/cli.py
@@ -1,13 +1,19 @@
+import argparse
 import os
 import shutil
 from django.core.management.utils import get_random_secret_key
 
-def create_project(name):
-    template_dir = os.path.join(os.path.dirname(__file__), "template")
+def create_project():
+    parser = argparse.ArgumentParser(description="Create Django base project")
+    parser.add_argument("name", help="Project name")
 
+    args = parser.parse_args()
+    name = args.name
+
+    template_dir = os.path.join(os.path.dirname(__file__), "template")
     shutil.copytree(template_dir, name)
 
-    # Create .env
+    # Generate .env with unique SECRET_KEY
     env_path = os.path.join(name, ".env")
     with open(env_path, "w") as f:
         f.write(f"DJANGO_SECRET_KEY={get_random_secret_key()}\n")
